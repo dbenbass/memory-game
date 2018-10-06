4c1df55522d53c74c912f15a0a981fa1b19193c0
Daniella Ben-Bassat <daniella@Daniellas-MacBook-Pro.local>
2018-10-06 00:06:51 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,13 @@
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInplay = ["queen", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cards.push('cardOne'); 
+cards.push('cardTwo');
+console.log("User flipped queen");
+console.log("User flipped king");
+ 
+if(cardsInplay.length === 2){console.log("two cards lol");}
+if(cardsInplay [0] === cardsInplay[1]){alert("You found a match");}
+else {alert("Sorry, try again.")}
