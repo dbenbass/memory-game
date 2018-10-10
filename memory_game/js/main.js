cdfce776354064d92d9fa3e340fa502eed2077ed
dbenbass <dbenbass@gmail.com>
2018-10-10 00:48:06 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,33 @@
 
 var cards = ["queen", "queen", "king", "king"];
-var cardsInplay = ["queen", "king"];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cards.push('cardOne'); 
-cards.push('cardTwo');
-console.log("User flipped queen");
-console.log("User flipped king");
+var cardsInPlay = [];
+
+
+var checkForMatch = function(){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+  }
+var flipCard = function(cardId)
+	{ 
+		cardsInPlay.push(cards[cardId]);
+		if (cardsInPlay.length === 2) {
+		checkForMatch();
+	 }
+		console.log("User flipped " + cards[cardId]);
  
-if(cardsInplay.length === 2){console.log("two cards lol");}
-if(cardsInplay [0] === cardsInplay[1]){alert("You found a match");}
-else {alert("Sorry, try again.")}
+}
+flipCard(0);
+flipCard(1);
+
+
+
+//flipCard(2);
+//var cardOne = cards[0];
+//var cardTwo = cards[2];
+//cardsInplay.push('cardOne'); 
+//cardsInplay.push('cardTwo');
+//console.log("User flipped" + cardOne);
+//console.log("User flipped" + cardTwo");
