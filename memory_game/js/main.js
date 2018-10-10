b527db0577d46d8f2a4307bbdd851c67581f8080
dbenbass <dbenbass@gmail.com>
2018-10-10 11:40:57 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,33 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+	}
+
+];
+
+
 var cardsInPlay = [];
-
-
 var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -12,15 +37,16 @@ var checkForMatch = function(){
   }
 var flipCard = function(cardId)
 	{ 
-		cardsInPlay.push(cards[cardId]);
+		cardsInPlay.push(cards[cardId].rank);
 		if (cardsInPlay.length === 2) {
 		checkForMatch();
 	 }
-		console.log("User flipped " + cards[cardId]);
- 
+		console.log("User flipped " + cards[cardId].rank);
+ 		console.log(cards[cardId].suit);
+ 		console.log(cards[cardId].cardImage);
 }
 flipCard(0);
-flipCard(1);
+flipCard(2);
 
 
 
